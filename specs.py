783ab9df094ab28dfb598f380d488864c4d4b322
Richard Weiss <richard.weiss.tuwien@gmail.com>
2024-11-21 14:00:06 +0100

    Update specs.py
    standardized specs handling

--- specs.py
+++ specs.py
@@ -39,3 +39,24 @@ def get_step_num_spec(step_num_max):
             name="step_num"))
 
 # ---------------------------------------------------------------- #
+
+def get_observation_action_spec_tabular(bounds):
+    obs_min, obs_max, act_min, act_max = bounds
+
+    return get_observation_action_spec(
+        obs_shape=(), act_shape=(),
+        obs_dtype=np.int32, act_dtype=np.int32,
+        obs_min=obs_min, obs_max=obs_max, act_min=act_min, act_max=act_max,
+    )
+
+def get_observation_action_spec_continuous(bounds, shapes):
+    obs_min, obs_max, act_min, act_max = bounds
+    obs_shape, act_shape = shapes
+
+    return get_observation_action_spec(
+        obs_shape=obs_shape, act_shape=act_shape,
+        obs_dtype=np.float32, act_dtype=np.int32,
+        obs_min=obs_min, obs_max=obs_max, act_min=act_min, act_max=act_max,
+    )
+
+# ---------------------------------------------------------------- #
