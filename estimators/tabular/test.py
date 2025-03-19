658ae49d2845471e6ec286eb97332d37a49a0176
Richard Weiss <richard.weiss.tuwien@gmail.com>
2025-03-19 16:48:16 +0100

    Update test.py
    reworked plotting auxiliary estimates

--- estimators/tabular/test.py
+++ estimators/tabular/test.py
@@ -15,7 +15,7 @@ def test_estimator(estimator_DICE, gamma, projected, weighted, **kwargs):
 
     # -------------------------------- #
 
-    d0_bar, dD_bar, P_bar, r_bar, n = estimator_DICE.auxiliary_estimates
+    d0_bar, dD_bar, P_bar, r_bar, n = estimator_DICE.auxiliary_estimates.bar
 
     d0_hat = d0_bar / n
     dD_hat = dD_bar / n
@@ -62,9 +62,12 @@ def test_auxiliary_estimates(auxiliary_estimates, title_prefix, dir=None):
         path = os.path.join(dir, fn)
         return path
 
+    color = "blue"
+    alpha = 0.25
+
     # -------------------------------- #
 
-    d0_bar, dD_bar, P_bar, r_bar, n = auxiliary_estimates
+    d0_bar, dD_bar, P_bar, r_bar, n = auxiliary_estimates.bar
 
     d0_hat = d0_bar / n
     dD_hat = dD_bar / n
@@ -76,8 +79,6 @@ def test_auxiliary_estimates(auxiliary_estimates, title_prefix, dir=None):
     dD_bar_mask = dD_bar[~mask]
     P_bar_mask = P_bar[~mask, :][:, ~mask]
     r_bar_mask = dD_bar[~mask]
-
-    auxiliary_estimates_mask = (d0_bar_mask, dD_bar_mask, P_bar_mask, r_bar_mask, n)
 
     # -------------------------------- #
 
@@ -107,15 +108,18 @@ def test_auxiliary_estimates(auxiliary_estimates, title_prefix, dir=None):
 
     # -------------------------------- #
 
-    _, ax = plt.subplots(nrows=3, figsize=(12, 9), sharex=True)
+    _, ax = plt.subplots(nrows=3, figsize=(12, 9), sharex=True, tight_layout=True)
 
-    ax[0].bar(range(len(d0_hat)), d0_hat)
-    ax[1].bar(range(len(dD_hat)), dD_hat)
-    ax[2].bar(range(len(r_hat)), r_hat)
+    ax[0].bar(range(len(d0_hat)), d0_hat, color=color, alpha=alpha)
+    ax[1].bar(range(len(dD_hat)), dD_hat, color=color, alpha=alpha)
+    ax[2].bar(range(len(r_hat)),  r_hat,  color=color, alpha=alpha)
 
     ax[0].set_ylabel(r"$\hat{d}_0(s, a)$")
     ax[1].set_ylabel(r"$\hat{d}^D(s, a)$")
     ax[2].set_ylabel(r"$\hat{r}  (s, a)$")
+
+    for i in range( len(ax) ):
+        ax[i].grid(linestyle=":")
 
     plt.xlabel(r"$(s, a)$-index")
 
@@ -158,9 +162,9 @@ def test_auxiliary_estimates(auxiliary_estimates, title_prefix, dir=None):
 
     # -------------------------------- #
 
-    def test_sum(auxiliary_estimates, names, mask=False, epsilon=1e-4):
+    def test_sum(bar, mask=False, epsilon=1e-4):
 
-        d0_bar, dD_bar, P_bar, r_bar, n = auxiliary_estimates
+        d0_bar, dD_bar, P_bar, r_bar, n = bar
 
         d0_hat = d0_bar / n
         dD_hat = dD_bar / n
@@ -186,7 +190,7 @@ def test_auxiliary_estimates(auxiliary_estimates, title_prefix, dir=None):
         ]
 
         plt.figure( figsize=(12, 4), )
-        bar = plt.bar(labels, values)
+        bar = plt.bar(labels, values, color=color, alpha=alpha)
         plt.bar_label(bar, label_type="center")
         space = r"S \times A" if not mask else r"(S \times A)^\text{mask}"
         matrix = r"\hat{P}^\pi" if not mask else r"\hat{P}^{\pi, \text{mask}}"
@@ -208,94 +212,16 @@ def test_auxiliary_estimates(auxiliary_estimates, title_prefix, dir=None):
 
         plt.show()
 
-
     test_sum(
-        auxiliary_estimates=auxiliary_estimates,
-        names=["d0_hat", "dD_hat", "P_hat"],
+        bar=(d0_bar, dD_bar, P_bar, r_bar, n),
         mask=False,
     )
 
     test_sum(
-        auxiliary_estimates=auxiliary_estimates_mask,
-        names=["d0_hat_mask", "dD_hat_mask", "P_hat_mask"],
+        bar=(d0_bar_mask, dD_bar_mask, P_bar_mask, r_bar_mask, n),
         mask=True,
     )
 
     print("#", "-"*64, "#", "\n")
 
-    # -------------------------------- #
-
-
-def get_non_trivial_transitions(auxiliary_estimates, n_clusters):
-    d0_bar, dD_bar, P_bar, r_bar, n = auxiliary_estimates
-
-    A = np.copy(P_bar[:-10, :-10])
-
-    for i in range(n_clusters):
-        k = i * 5
-        l = k + 5
-        A[k:l, k:l] = 1
-
-    absolute = np.sum(A != 0) - 5*5*n_clusters
-    relative = absolute / (n_clusters + 1) ** 2
-
-    return { "absolute": absolute, "relative": relative, }
-
-# ---------------------------------------------------------------- #
-
-# deprecated
-
-def get_indices_bad_matrix(auxiliary_estimates):
-
-    d0_bar, dD_bar, P_bar, r_bar, n = auxiliary_estimates
-
-    indices_bad_list = []
-    indices_bad_list_T = []
-
-    for i in range(len(dD_bar)):
-        for j in range(len(dD_bar)):
-            if dD_bar[i] == 0:
-                if P_bar  [i, j] != 0: indices_bad_list  .append([i, j])
-                if P_bar.T[i, j] != 0: indices_bad_list_T.append([i, j])
-
-    def get_indices_bad_df(matrix, indices_bad_list, col1="from", col2="to"):
-        i_array = []; j_array = []; v_array = []
-
-        for i, j in indices_bad_list:
-            obs_act_i = index_to_obs_act(i)
-            assert action_masks_test[obs_act_i]
-            i_array.append(obs_act_i)
-
-            obs_act_j = index_to_obs_act(j)
-            assert action_masks_test[obs_act_j]
-            j_array.append(obs_act_j)
-
-            v_array.append( matrix[i, j] )
-
-        return pd.DataFrame({ col1: i_array, col2: j_array, "v": v_array, })
-    
-    df   = get_indices_bad_df(P_bar,   indices_bad_list,    col1="from", col2="to")
-    df_T = get_indices_bad_df(P_bar.T, indices_bad_list_T,  col1="to",   col2="from")
-
-    return df, df_T
-
-def get_indices_bad_mask(auxiliary_estimates):
-    d0_bar, dD_bar, P_bar, r_bar, n = auxiliary_estimates
-
-    elements = []
-
-    for o, m in enumerate(action_masks_test):
-        for a, v in enumerate(m):
-            if v:
-                i = obs_act_to_index(o, a)
-                elements.append( (o, a, int(dD_bar[i])) )
-
-    return [(o, a) for o, a, number in elements if number == 0]
-
-def display_indices_bad(auxiliary_estimates):
-    indices_bad_matrix, indices_bad_matrix_T = get_indices_bad_matrix(auxiliary_estimates)
-    indices_bad_mask = get_indices_bad_mask(auxiliary_estimates)
-
-    display(indices_bad_matrix_T) # type: ignore
-    display(indices_bad_mask)     # type: ignore
 # ---------------------------------------------------------------- #
