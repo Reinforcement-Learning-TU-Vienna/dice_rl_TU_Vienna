e3a072cf8dae95ea2427988180d4e3d19479104b
Richard Weiss <richard.weiss.tuwien@gmail.com>
2025-03-19 16:52:18 +0100

    Update tabular.py
    - Remove plot_sr_pv_error
    - Update plot_pv & plot_sdc_error: Add plot: using plot_infos

--- plot/tabular.py
+++ plot/tabular.py
@@ -1,246 +1,92 @@
 # ---------------------------------------------------------------- #
 
-import numpy as np
+import os
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-import os
-
-from dice_rl_TU_Vienna.estimators.get import get_gammas_log10
-
-from dice_rl_TU_Vienna.utils.general import safe_zip
-
 # ---------------------------------------------------------------- #
 
-def plot_sr_pv_error(suptitle, success_rates, estimators, dir=None):
-    gammas = get_gammas_log10()
-
-    plt.figure()
-
-    x = 1 - gammas
-    markers = [".", "v", "^"]
-    colors = ["black", "lightgrey", "grey"]
-
-    for z in safe_zip(estimators, success_rates, colors, markers):
-        estimator, success_rate, color, marker = z
-
-        y = [ np.abs( success_rate - estimator(gamma, scale=False) ) for gamma in gammas ]
-        label = estimator.__name__
-        plt.plot(x, y, label=label, color=color, marker=marker)
-
-    plt.plot(x, x, label=r"$O(x)$", color="black", linestyle=":")
-
-    plt.grid(linestyle=":")
-
-    plt.suptitle(suptitle)
-    plt.xlabel(r"$1 - \gamma$")
-    plt.ylabel(r"$| \hat{\rho}^\pi(\gamma) \div (1 - \gamma) - \sigma |$")
-    plt.legend()
-
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.gca().invert_xaxis()
-
-    if dir is not None:
-
-        if not tf.io.gfile.isdir(dir):
-            tf.io.gfile.makedirs(dir)
-
-        file_name = "sr_pv_error"
-        save_path = os.path.join(dir, file_name)
-
-        plt.savefig(save_path, bbox_inches="tight")
-
-    plt.show()
-
-# -------------------------------- #
-
-def plot_pvs(
-        estimators,
-        gammas,
-        projected, weighted,
-        modified, lamda,
-        #
-        pvs, pv_lims, pv_ref, pv_lim_ref,
-        labels,
-        colors, colors_lim,
-        markers, markers_lim,
+def plot(
+        plot_infos,
         suptitle,
-        one_minus_gamma, errors=False,
+        one_minus_gamma=True, scale_pv=False,
         xlabel=None, ylabel=None,
-        scale_x=False, scale_y=False,
-        scale_pv=False,
-        ylim=None, legend=True,
-        dir=None):
+        xlim=None, ylim=None,
+        xscale=True, yscale=False,
+        legend=True,
+        dir=None,
+    ):
 
-    if errors:
-        assert pv_ref is not None
-        if pv_lims is not None:
-            assert pv_lim_ref is not None
+    plt.figure(tight_layout=True)
+    plt.suptitle(suptitle)
 
-    plt.figure()
+    for plot_info in plot_infos:
+        A = "x" in plot_info.keys()
+        B = "y" in plot_info.keys()
+        if A and B:
+            plt.plot(
+                plot_info["x"] if not one_minus_gamma else 1 - plot_info["x"],
+                plot_info["y"] if not scale_pv else plot_info["y"] / (1 - plot_info["x"]),
+                label=plot_info["label"],
+                color=plot_info["color"],
+                marker=plot_info["marker"],
+            )
 
-    x = gammas if not one_minus_gamma else 1 - gammas
+        else:
+            if A:
+                plt.axvline(
+                    plot_info["x"] if not one_minus_gamma else 1 - plot_info["x"],
+                    label=plot_info["label"],
+                    color=plot_info["color"],
+                    marker=plot_info["marker"],
+                    linestyle=":",
+                )
+            if B:
+                plt.axhline(
+                    plot_info["y"],
+                    label=plot_info["label"],
+                    color=plot_info["color"],
+                    marker=plot_info["marker"],
+                    linestyle=":",
+                )
 
-    # -------------------------------- #
-
-    if pv_lims is not None:
-
-        Z = pv_lims, colors_lim, markers_lim
-        for z in safe_zip(*Z):
-            pv_lim, color, marker = z
-
-            y = pv_lim if not errors else np.abs(pv_lim - pv_lim_ref)
-
-            plt.axhline(y, color=color, marker=marker, linestyle=":")
-
-    # ---------------- #
-
-    if labels is None: labels = [None] * len(pvs)
-
-    Z = estimators, pvs, labels, colors, markers
-    for z in safe_zip(*Z):
-        estimator, pv, label, color, marker = z
-
-        y1 = pv if not errors else np.abs(pv - pv_ref)
-        y2 = 1 / (1 - gammas) if scale_pv else 1
-        y = y1 * y2
-
-        if label is None:
-            label = estimator.__name__
-            if label == "TabularDice":         label += f", {modified=}"
-            if label == "TabularGradientDice": label += f", {lamda=}"
-
-        if label == "": label = None
-
-        plt.plot(x, y, label=label, color=color, marker=marker)
-
-    # -------------------------------- #
-
-    if scale_x: plt.xscale("log")
-    if scale_y: plt.yscale("log")
+    if xscale: plt.xscale("log")
+    if yscale: plt.yscale("log")
 
     if one_minus_gamma: plt.gca().invert_xaxis()
 
-    if xlabel is None: xlabel = \
-        r"$\gamma$" if not one_minus_gamma else r"$1 - \gamma$"
-    plt.xlabel(xlabel)
+    if xlabel is None:
+        x = r"\gamma"
+        if one_minus_gamma: x = f"1 - {x}"
+        xlabel = f"${x}$"
 
     if ylabel is None:
-        x = r"$\rho^\pi(\gamma)$"
-        y = r"$| \hat{\rho}^\pi(\gamma) - \rho^\pi(\gamma) |$"
-        ylabel = x if not errors else y
-        if scale_pv:
-            x = ylabel[1:-1]
-            y = r"\div (1 - \gamma)"
-            ylabel = f"${x} {y}$"
-    plt.ylabel(ylabel)
+        ylabel = ""
 
-    if legend: plt.legend()
-    plt.suptitle(suptitle)
-    title = f"{projected=}, {weighted=}"
-    plt.title(title)
-
-    plt.ylim(ylim)
-    plt.grid(linestyle=":")
-
-    # -------------------------------- #
-
-    if dir is not None:
-
-        if not tf.io.gfile.isdir(dir):
-            tf.io.gfile.makedirs(dir)
-
-        file_name = f"{suptitle}; {title}; {legend=}"
-        save_path = os.path.join(dir, file_name)
-
-        plt.savefig(save_path, bbox_inches="tight")
-
-    # -------------------------------- #
-
-    plt.show()
-
-
-def plot_sdc_errors(
-        estimators,
-        gammas,
-        projected,
-        modified, lamda,
-        #
-        sdcs, sdc_lims, sdc_ref, sdc_lim_ref,
-        colors, colors_lim,
-        markers, markers_lim,
-        suptitle,
-        one_minus_gamma,
-        scale_x=False, scale_y=False, ylim=None, legend=True,
-        dir=None):
-
-    if sdc_lims is not None:
-        assert sdc_lim_ref is not None
-
-    plt.figure()
-
-    x = gammas if not one_minus_gamma else 1 - gammas
-
-    # -------------------------------- #
-
-    if sdc_lims is not None:
-
-        Z = sdc_lims, colors_lim, markers_lim
-        for z in safe_zip(*Z):
-            sdc_lim, color, marker = z
-
-            y = np.mean( (sdc_lim - sdc_lim_ref) ** 2 )
-            plt.axhline(y, color=color, marker=marker, linestyle=":")
-
-    # ---------------- #
-
-    Z = estimators, sdcs, colors, markers
-    for z in safe_zip(*Z):
-        estimator, sdc, color, marker = z
-
-        y = np.mean( (sdc - sdc_ref) ** 2, axis=-1 )
-
-        label = estimator.__name__
-        if label == "TabularDice":         label += f", {modified=}"
-        if label == "TabularGradientDice": label += f", {lamda=}"
-
-        plt.plot(x, y, label=label, color=color, marker=marker)
-
-    # -------------------------------- #
-
-    if scale_x: plt.xscale("log")
-    if scale_y: plt.yscale("log")
-
-    if one_minus_gamma: plt.gca().invert_xaxis()
-
-    xlabel = r"$\gamma$" if not one_minus_gamma else r"$1 - \gamma$"
     plt.xlabel(xlabel)
-
-    ylabel = r"$\mathbb{E}_D | \hat{w}_{\pi / D}(\gamma) - w_{\pi / D}(\gamma) |^2$"
     plt.ylabel(ylabel)
 
-    if legend: plt.legend()
-    plt.suptitle(suptitle)
-    title = f"{projected=}"
-    plt.title(title)
-
+    plt.xlim(xlim)
     plt.ylim(ylim)
-    plt.grid(linestyle=":")
 
-    # -------------------------------- #
+    if legend:
+        if   isinstance(legend, bool): loc = "best"
+        elif isinstance(legend, dict): loc = legend.get("loc", "best")
+        else: raise TypeError
+        plt.legend(loc=loc)
+
+    plt.grid(linestyle=":")
 
     if dir is not None:
 
         if not tf.io.gfile.isdir(dir):
             tf.io.gfile.makedirs(dir)
 
-        file_name = f"{suptitle}; {title}; {legend=}"
+        file_name = suptitle.replace("\n", "; ")
         save_path = os.path.join(dir, file_name)
 
         plt.savefig(save_path, bbox_inches="tight")
-
-    # -------------------------------- #
 
     plt.show()
 
